ce3ea13e9622be1379382396530f56d38590edaf
jermz678 <jermz678@live.com>
2021-05-16 18:37:17 -0500

    created router.delete

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 var uniqid = require('uniqid');
-const notes = require('../db/db');
+
+let notes = require('../db/db');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
-}) 
+});
 
 router.post('/notes', (req, res) => {
     let request = req.body;
@@ -17,9 +18,19 @@ router.post('/notes', (req, res) => {
         path.join(__dirname, '../db/db.json'), 
         JSON.stringify(notes)
     );
+})
 
-
-
+router.delete('/notes/:id', (req, res) => {
+    const { id }  = req.params;
+    const deletedID = notes.find(notes => notes.id === id);
+    if (deletedID){
+        notes = notes.filter(notes => notes.id != id);
+        res.redirect('/notes')
+        res.status(200);
+        
+    }else{
+        res.status(404);
+    }
 })
 
 module.exports = router;
