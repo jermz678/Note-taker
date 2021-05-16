2e81389c14c00f42319f4af7ebb58b54837302cc
jermz678 <jermz678@live.com>
2021-05-16 12:18:49 -0500

    created route to get to note page

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 var uniqid = require('uniqid');
 const app = express();
 const { notes } = require('./Develop/db/db.json')
 
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'))
+})
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
